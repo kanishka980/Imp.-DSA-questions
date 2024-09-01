c94099153b8d81c4be2f0b9ec124a64974f6f379
Kanishka Sharma <105300956+kanishka980@users.noreply.github.com>
2024-09-01 22:16:11 +0530

    Create triangle.cpp

---
+++ dynamic problem/triangle.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n=triangle.size();
+        vector<vector<int>> dp(n+1,vector<int>(n+1));
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int j=0;j<=i;j++)
+            {
+                dp[i][j]=triangle[i][j]+min(dp[i+1][j],dp[i+1][j+1]);
+            }
+        }
+        return dp[0][0];
+
+        
+    }
+};
